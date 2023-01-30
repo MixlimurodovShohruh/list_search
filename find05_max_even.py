8b5d0f5281db92d727b4fd46fe38410d47a24ba2
MixlimurodovShohruh <MixlimurodovShohruhElyorivech@gmail.com>
2023-01-30 19:21:39 +0500

    found

--- find05_max_even.py
+++ find05_max_even.py
@@ -9,10 +9,12 @@ def find_max_even(data):
     for i in data:
         if i%2==0:
             d.append(i)
-    mx=d[0]
+    if len(d)==0:
+        return -1
+    mx=data[0]
     for d in d:
         if mx<d:
             mx=d
 
     return mx
-print(find_max_even([7, 6, 3, 4, 9]))
+print(find_max_even([1, 4, 3, 8, 5]))
--- find06_max_odd.py
+++ find06_max_odd.py
@@ -9,9 +9,12 @@ def find_max_odd(data):
     for i in data:
         if i%2==1:
             g.append(i)
+    if len(g)==0:
+        return -1
     mx=g[0]
     for c in g:
         if mx<c:
             mx=c
+    
     return mx
 print(find_max_odd([11, 7, 5, 4, 9]))
